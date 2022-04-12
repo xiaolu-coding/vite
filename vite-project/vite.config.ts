bc5d8af11e1b19d35fdc71ac47cf440fa246a593
xiaolu-coding <454819375@qq.com>
2022-04-12 15:29:18 +0800

    chore: scss文件自动导入

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,15 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import react from '@vitejs/plugin-react'
 // 引入 path 包注意两点:
 // 1. 为避免类型报错，你需要通过 `pnpm i @types/node -D` 安装类型
 // 2. tsconfig.json 中设置 `allowSyntheticDefaultImports: true`，以允许下面的 default 导入方式
-// import path from "path"
+import path from "path"
+
+// 全局 scss 文件的路径
+// 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // root: path.join(__dirname, 'src'),
   build: {
-    manifest: true
-  }
+    manifest: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+  },
 })
