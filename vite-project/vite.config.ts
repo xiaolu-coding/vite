9bb11ec7dc7b23e8db3e30a1c5d4c175427e76af
xiaolu-coding <454819375@qq.com>
2022-04-12 15:40:27 +0800

    chore: postCSS配置

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // 1. 为避免类型报错，你需要通过 `pnpm i @types/node -D` 安装类型
 // 2. tsconfig.json 中设置 `allowSyntheticDefaultImports: true`，以允许下面的 default 导入方式
 import path from "path"
+// pnpm i autoprefixer -D 
+// postCss插件
+import autoprefixer from 'autoprefixer';
 
 // 全局 scss 文件的路径
 // 用 normalizePath 解决 window 下的路径问题
@@ -27,6 +30,14 @@ export default defineConfig({
       // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
       // 其中，name 表示当前文件名，local 表示类名
       generateScopedName: "[name]__[local]___[hash:base64:5]",
+    },
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ["Chrome > 40", "ff > 31", "ie 11"],
+        }),
+      ],
     },
   },
 })
