c8e24a30c1bcb189bcd86c4333119a2a78d67da7
xiaolu-coding <454819375@qq.com>
2022-04-12 15:42:01 +0800

    chore: CSS in JS

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -14,7 +14,22 @@ const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+     babel: {
+        // 加入 babel 插件
+        // 以下插件包都需要提前安装
+        // 当然，通过这个配置你也可以添加其它的 Babel 插件
+        plugins: [
+          // 适配 styled-component
+          "babel-plugin-styled-components",
+          // 适配 emotion
+          "@emotion/babel-plugin"
+        ]
+      },
+      // 注意: 对于 emotion，需要单独加上这个配置
+      // 通过 `@emotion/react` 包编译 emotion 中的特殊 jsx 语法
+      jsxImportSource: "@emotion/react"
+  })],
   // root: path.join(__dirname, 'src'),
   build: {
     manifest: true,
