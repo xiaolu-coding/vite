6d3320921eb12e2dde94421e24c843347452014e
xiaolu-coding <454819375@qq.com>
2022-04-12 15:25:27 +0800

    chore: 自带的sass预处理 安装即用

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 // 引入 path 包注意两点:
 // 1. 为避免类型报错，你需要通过 `pnpm i @types/node -D` 安装类型
 // 2. tsconfig.json 中设置 `allowSyntheticDefaultImports: true`，以允许下面的 default 导入方式
-import path from "path"
+// import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: path.join(__dirname, 'src'),
+  // root: path.join(__dirname, 'src'),
   build: {
     manifest: true
   }
