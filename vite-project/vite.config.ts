6ca03b2b378f429ea800e2776585ac535d5b8b82
xiaolu-coding <454819375@qq.com>
2022-04-12 15:33:11 +0800

    chore: css Modules配置

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -23,5 +23,10 @@ export default defineConfig({
         additionalData: `@import "${variablePath}";`,
       },
     },
+    modules: {
+      // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
+      // 其中，name 表示当前文件名，local 表示类名
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
+    },
   },
 })
